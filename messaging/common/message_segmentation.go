8cce1d9e7a6981395af35268ce5b80510e6b2ab2
kaichaosun <kaichaosuna@gmail.com>
2025-06-25 19:11:38 +0800

    chore_: test protobuf miss decoding

--- messaging/common/message_segmentation.go
+++ messaging/common/message_segmentation.go
@@ -33,6 +33,7 @@ func (s *MessageSender) segmentMessage(newMessage *wakutypes.NewMessage) ([]*wak
 	// We set the max message size to 3/4 of the allowed message size, to leave
 	// room for segment message metadata.
 	newMessages, err := segmentMessage(newMessage, int(s.transport.MaxMessageSize()/4*3))
+	s.logger.Debug("segmenting message ---===== payload", zap.String("Payload", types.Bytes2Hex(newMessage.Payload)))
 	s.logger.Debug("message segmented", zap.Int("segments", len(newMessages)))
 	for i := range newMessages {
 		var segmentMessage protobuf.SegmentMessage
@@ -160,6 +161,9 @@ func (s *MessageSender) handleSegmentationLayer(message *types.Message) error {
 	segmentMessage := &types.SegmentMessage{
 		SegmentMessage: &protobuf.SegmentMessage{},
 	}
+
+	logger.Debug("unmarshalling SegmentMessage from payload",
+		zap.Int("PayloadSize", len(message.TransportLayer.Payload)), zap.String("Payload", types.Bytes2Hex(message.TransportLayer.Payload)))
 	err := proto.Unmarshal(message.TransportLayer.Payload, segmentMessage.SegmentMessage)
 	if err != nil {
 		return errors.Wrap(err, "failed to unmarshal SegmentMessage")
